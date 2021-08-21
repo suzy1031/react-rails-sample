bbb5477b1540beb8b960a0103cb69cae415bc897
koheisugawra <51276845+suzy1031@users.noreply.github.com>
2021-08-22 01:18:12 +0900

    add dispatch action

--- frontend/src/components/users/UserPost.jsx
+++ frontend/src/components/users/UserPost.jsx
@@ -1,54 +1,46 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
 // style
 import { Button } from '@material-ui/core';
-// api
-import { getUserPosts } from '../../lib/api/user';
-import { deletePost } from '../../lib/api/post';
-// context
-import { AuthContext } from '../../App';
 // component
 import SpaceRow from '../layout/SpaceRow';
 import ListTable from '../commons/ListTable';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import allActions from '../../redux/actions/allActions';
 
 const UserPost = () => {
-  const { loading, isSignedIn, currentUser } = useContext(AuthContext);
-  const [userPosts, setUserPosts] = useState({});
+  const dispatch = useDispatch();
+  const asyncCurrentUser = useSelector(
+    (state) => state.asyncCurrentUser
+  ).payload;
+  const asyncUserPosts = useSelector(
+    (state) => state.asyncUserPostsData
+  ).payload;
   const history = useHistory();
 
   useEffect(() => {
-    handleGetUserPosts();
-  }, [currentUser]);
-
-  const handleGetUserPosts = async () => {
-    if (!loading) {
-      if (isSignedIn) {
-        const res = await getUserPosts(currentUser.id);
-        console.table(res.data);
-        setUserPosts(res.data);
-      } else {
-        <Redirect to='/signin' />;
-      }
+    dispatch(allActions.getAsyncCurrentUser());
+    if (asyncCurrentUser?.data.id) {
+      dispatch(allActions.getAsyncUserPostsData(asyncCurrentUser?.data.id));
+    } else {
+      <Redirect to='/signin' />;
     }
-  };
+  }, []);
 
   const handleDelete = async (item) => {
-    console.log('click', item.id);
-    try {
-      const res = await deletePost(item.id);
-      console.log(res.data);
-      handleGetUserPosts();
-    } catch (e) {
-      console.log(e);
-    }
+    const id = item.id;
+    dispatch(allActions.deleteAsyncData(id));
+    history.go(0);
   };
+
   const UserTable = () => {
-    if (userPosts.length >= 1) {
+    if (asyncUserPosts?.length >= 1) {
       return (
         <ListTable
-          dataList={userPosts}
+          dataList={asyncUserPosts}
           handleDelete={handleDelete}
-          currentUser={currentUser}
+          currentUser={asyncCurrentUser?.data}
         />
       );
     } else {
@@ -58,7 +50,7 @@ const UserPost = () => {
 
   return (
     <>
-      <h1>{currentUser.name}の投稿一覧</h1>
+      <h1>{asyncCurrentUser?.data.name}の投稿一覧</h1>
       <Button
         variant='contained'
         color='primary'
