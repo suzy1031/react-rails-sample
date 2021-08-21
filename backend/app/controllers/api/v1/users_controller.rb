e9b53e6bd6c3cc07a1b129824c7d9b2c115bcd10
koheisugawra <51276845+suzy1031@users.noreply.github.com>
2021-08-22 00:37:18 +0900

    add before_action

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_api_v1_user!, only: [:show]
   def show
     render json: Post.where(user_id: params[:id]).to_json(include: :detail_info)
   end
